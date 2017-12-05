a753dfe6c8ce3cf2cfaa20153e1fb6ffffa86e41
bmintz <bmintz@users.noreply.github.com>
2017-12-05 00:28:27 -0600

    5: fix tabs

--- 3/spiral_memory.py
+++ 3/spiral_memory.py
@@ -9,22 +9,22 @@ import sys
 
 
 def get_coordinate(n):
-		k=ceil((sqrt(n)-1)/2)
-		t=2*k+1
-		m=t**2
-		t=t-1
-		if n >= m - t:
-			return k - (m-n), -k
-		else:
-			m -= t
-		if n >= m - t:
-			return -k, -k+(m-n)
-		else:
-			m -= t
-		if n >= m-t:
-			return -k+(m-n), k
-		else:
-			return k, k-(m-n-t)
+	k=ceil((sqrt(n)-1)/2)
+	t=2*k+1
+	m=t**2
+	t=t-1
+	if n >= m - t:
+		return k - (m-n), -k
+	else:
+		m -= t
+	if n >= m - t:
+		return -k, -k+(m-n)
+	else:
+		m -= t
+	if n >= m-t:
+		return -k+(m-n), k
+	else:
+		return k, k-(m-n-t)
 
 
 def ismutable(x):
