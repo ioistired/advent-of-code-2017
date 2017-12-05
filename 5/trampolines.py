61202848c7d26cd837d9be022c2d843a0feb73d9
bmintz <bmintz@users.noreply.github.com>
2017-12-04 23:44:00 -0600

    solve day 5

---
+++ 5/trampolines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+
+from ben import input_iter
+def answer(instrs, day2=False):
+	instrs = list(map(int, conv(instrs.split('\n'))))
+	pos = 0
+	i = 0
+	while pos < len(instrs):
+		old_pos = pos
+		jmp = instrs[pos]
+		pos += instrs[pos]
+		if jmp >= 3 and day2:
+			instrs[old_pos] -= 1
+		else:
+			instrs[old_pos] += 1
+
+		i += 1
+
+	return i
+
+def conv(l):
+	return (int(x) for x in l if x)
+
+def main():
+	if len(sys.argv) == 1:
+		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
+		sys.exit(1)
+
+	print(answer(sys.stdin.read(), sys.argv[1] == '2'))
+
+
+if __name__ == '__main__':
+	main()
