3238f4226ff1139999eafc7c1a871c5c0e7e93c2
bmintz <bmintz@users.noreply.github.com>
2017-12-09 00:53:39 -0600

    fuck it, just completely copy day 7 code :(

--- 07/circus.py
+++ 07/circus.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+import collections
 import sys
 
 import networkx as nx
+from ben import input_iter
 
-def answer(input):
+
+# https://www.reddit.com/r/adventofcode/comments/7i44pg/2017_day_7_solutions/dqw0f0c/
+def answer(input, part2):
 	graph = nx.DiGraph()
 
-	for line in input.split('\n'):
+	for line in input:
 		if not line:
 			continue
 		line = line.split()
-		print(line)
 		name = line[0]
 		graph.add_node(name, weight=int(line[1].strip('()')))
 
@@ -23,18 +26,43 @@ def answer(input):
 				graph.add_edge(name, child)
 
 	ordered = list(nx.topological_sort(graph))
-	return ordered[0]
+	if not part2:
+		return ordered[0]
+
+	weights = {}
+	# Going backwards (starting from the leaves)
+	for node in reversed(ordered):
+		# Start with this nodes own weight
+		total = graph.nodes[node]['weight']
+
+		counts = collections.Counter(weights[child] for child in graph[node])
+		unbalanced = None
+
+		for child in graph[node]:
+			# If this child's weight is different than others, we've found it
+			if len(counts) > 1 and counts[weights[child]] == 1:
+				unbalanced = child
+				break
+
+			# Otherwise add to the total weight
+			val = weights[child]
+			total += weights[child]
+
+		if unbalanced:
+			# Find the weight adjustment and the new weight of this node
+			diff = weights[unbalanced] - val
+			return graph.nodes[unbalanced]['weight'] - diff
+
+		# Store the total weight of the node
+		weights[node] = total
+
 
 
 def main():
 	if len(sys.argv) == 1:
 		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
 		sys.exit(1)
-	mode = sys.argv[1]
-	if mode == '1':
-		print(answer(sys.stdin.read()))
-	elif mode == '2':
-		...
+	print(answer(input_iter(), sys.argv[1] == '2'))
 
 
 if __name__ == '__main__':
