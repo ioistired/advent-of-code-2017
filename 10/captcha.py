5f6cca482618b5294379b31ee99d1c814f066676
bmintz <bmintz@users.noreply.github.com>
2017-12-09 23:54:57 -0600

    solve day 10 part 1

---
+++ 10/captcha.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+class CircularList(list):
+	def __getitem__(self, x):
+		if isinstance(x, slice):
+			return CircularList([self[x] for x in self._rangeify(x)])
+		return super().__getitem__(x % len(self))
+
+	def _rangeify(self, slice):
+		start, stop, step = slice.start, slice.stop, slice.step
+		if start is None:
+			start = 0
+		if stop is None:
+			stop = len(self)
+		if step is None:
+			step = 1
+		if step < 0:
+			start, stop = stop-1, start-1
+		return range(start, stop, step)
+
+def captcha(x, step):
+	t = CircularList(map(int, x))
+
+	sum = 0
+	for i, digit in enumerate(t):
+		if digit == t[i+step]:
+			sum += t[i]
+
+	return sum
+
+
+def main():
+	import sys
+	if sys.argv[1] == '1':
+		print(captcha(input(), 1))
+	elif sys.argv[1] == '2':
+		x = input()
+		print(captcha(x, len(x)//2))
+	else:
+		print(
+			'Need one or two depending on which part to solve.',
+			file=sys.stderr
+		)
+
+
+if __name__ == '__main__':
+	main()
---
+++ 10/template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+
+from captcha import CircularList
+
+def answer(lens, day2, list_size=256):
+	l = CircularList(x for x in range(list_size))
+	lens = list(map(int, lens.split(',')))
+	current_pos = 0
+	skip_size = 0
+
+	for length in lens:
+		l[current_pos:current_pos+length] = l[current_pos+length:current_pos:-1]
+		current_pos += length + skip_size
+		skip_size += 1
+	return l[0]*l[1]
+
+
+def main():
+	if len(sys.argv) == 1:
+		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
+		sys.exit(1)
+	print(answer(sys.stdin.read(), sys.argv[1] == '2'))
+
+
+if __name__ == '__main__':
+	main()
