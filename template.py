38ce4faf01f97ac143fda6e815770d452dc1bf06
bmintz <bmintz@users.noreply.github.com>
2017-12-09 23:59:53 -0600

    template.py: update to reflect how i usually write the solutions
    Usually it's not done in two separate functions, so it's easier to
    just have the answer take a boolean day2 parameter.
    
    I also included two versions of the print call, depending upon whether the
    input is line-based or fits in a single line.
    
    Just remove whichever one doesn't fit today's puzzle.

--- template.py
+++ template.py
@@ -3,7 +3,10 @@
 
 import sys
 
-def answer():
+from ben import input_iter
+
+
+def answer(data, day2):
 	...
 
 
@@ -11,12 +14,8 @@ def main():
 	if len(sys.argv) == 1:
 		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
 		sys.exit(1)
-	mode = sys.argv[1]
-	if mode == '1':
-		...
-	elif mode == '2':
-		...
-	print(answer(,))
+	print(answer(sys.stdin.read(), sys.argv[1] == '2')
+	print(answer(input_iter(), sys.argv[1] == '2')
 
 
 if __name__ == '__main__':
