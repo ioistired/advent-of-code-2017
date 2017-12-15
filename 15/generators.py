99e68eb8af625d3041f5dc30b64f510498aed40d
bmintz <bmintz@users.noreply.github.com>
2017-12-15 02:23:08 -0600

    solve day 15!!

--- 15/generators.py
+++ 15/generators.py
@@ -6,34 +6,45 @@ import sys
 from ben import input_iter
 
 
+def do_until(x, func, condition):
+	while True:
+		x = func(x)
+		if condition(x):
+			break
+	return x
+
+
+def gen(start, factor, multiple=1):
+	# an actual generator lol
+	x = start
+	func = lambda x: x*factor % (2**31-1)
+	condition = lambda x: x % multiple == 0
+	while True:
+		x = do_until(x, func, condition)
+		yield x
+
+
 def answer(data, day2):
 	data = data.split('\n')
-	a = int(data[0].split()[-1])
-	b = int(data[1].split()[-1])
-	a_factor = 16807
-	b_factor = 48271
+
+	a = gen(int(data[0].split()[-1]), 16807, 4 if day2 else 1)
+	b = gen(int(data[1].split()[-1]), 48271, 8 if day2 else 1)
 
 	score = 0
-	for i in range(40_000_000):
+	for i in range(5_000_000 if day2 else 40_000_000):
 		print(i, file=sys.stderr, end='\b'*len(str(i)))
-		a, b = next(a, a_factor), next(b, b_factor)
-		if cmp(a, b):
+		if cmp(next(a), next(b)):
 			score += 1
-	if day2:
-		...
+	# sometimes the status line isn't cleared fully
+	# leading to wrong answers printed
+	print()
+
 	return score
 
 
 def cmp(a, b):
-	def to_str(x):
-		return bin(x)[2:]
+	return a & 0xFFFF == b & 0xFFFF
 
-	a, b = to_str(a), to_str(b)
-	return a[-15:] == b[-15:]
-
-
-def next(x, factor):
-	return x * factor % (2**31-1)
 
 
 def main():
