7d8951d51973f5a30401090de0a68f317185b0f5
bmintz <bmintz@users.noreply.github.com>
2017-12-17 01:07:18 -0600

    start day 17

---
+++ 17/spinlock.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+
+
+def answer(data, day2):
+	...
+
+
+def main():
+	if len(sys.argv) == 1:
+		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
+		sys.exit(1)
+	print(answer(sys.stdin.read(), sys.argv[1] == '2'))
+
+
+if __name__ == '__main__':
+	main()
