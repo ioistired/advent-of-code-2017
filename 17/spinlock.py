53249b3c863a3cc6756d42b1f70c141115cdc1cb
bmintz <bmintz@users.noreply.github.com>
2017-12-17 02:14:19 -0600

    17: solve part 1

--- 17/spinlock.py
+++ 17/spinlock.py
@@ -3,9 +3,20 @@
 
 import sys
 
+from ben import CircularList
 
-def answer(data, day2):
-	...
+
+def answer(step_size, part2):
+	step_size = int(step_size)
+	buffer = [0]
+	pos = 0
+	for i in range(1, 2018):
+		pos = (pos+step_size) % len(buffer) + 1
+		buffer.insert(pos, i)
+
+	if part2:
+		...
+	return buffer[buffer.index(2017)+1]
 
 
 def main():
