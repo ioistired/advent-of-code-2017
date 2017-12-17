7cd4c06aa1a188a81980145e0284d67337008685
bmintz <bmintz@users.noreply.github.com>
2017-12-17 02:36:00 -0600

    17: solve part 2

--- 17/spinlock.py
+++ 17/spinlock.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+from collections import deque
 import sys
 
 from ben import CircularList
 
 
-def answer(step_size, part2):
+def part1(step_size):
 	step_size = int(step_size)
-	buffer = [0]
+	buffer = deque([0])
 	pos = 0
 	for i in range(1, 2018):
 		pos = (pos+step_size) % len(buffer) + 1
 		buffer.insert(pos, i)
 
-	if part2:
-		...
 	return buffer[buffer.index(2017)+1]
+
+
+def part2(step_size):
+	step_size = int(step_size)
+	length = 1
+	when = 0
+	pos = 0
+	for i in range(50_000_001):
+		pos = (pos+step_size) % length + 1
+		length += 1
+		if pos == 1:
+			when = i
+	return when+1
 
 
 def main():
 	if len(sys.argv) == 1:
 		print('Usage:', sys.argv[0], '<1|2> < input', file=sys.stderr)
 		sys.exit(1)
-	print(answer(sys.stdin.read(), sys.argv[1] == '2'))
+	print((part2 if sys.argv[1] == '2' else part1)(sys.stdin.read()))
 
 
 if __name__ == '__main__':
